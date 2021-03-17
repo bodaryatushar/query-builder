5f3629531329d57bc6620e68eab88f74760dc1fa
Tushar Bodarya <tushar.bodarya85@gmail.com>
2021-03-17 22:45:10 +0530

    Updates

---
+++ precache-manifest.885abd34a368163178f7a214a958717c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043e2ab2e6a4925e46767165a8c36ea7",
+    "url": "/template-renderer/index.html"
+  },
+  {
+    "revision": "7b00cc37090ebb856dea",
+    "url": "/template-renderer/static/js/2.467e670f.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/template-renderer/static/js/2.467e670f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1075f3874a0d8471d1f8",
+    "url": "/template-renderer/static/js/main.4c35e98a.chunk.js"
+  },
+  {
+    "revision": "e65dcc1382df22702201",
+    "url": "/template-renderer/static/js/runtime-main.2b1b2ea7.js"
+  }
+]);
